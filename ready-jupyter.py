32021c4d6163ab5f4f219f48a6fa56fd92670bb6
Francisco <fp.ob>
2020-11-16 17:18:38 +0100

    help function

--- ready-jupyter.py
+++ ready-jupyter.py
@@ -2,6 +2,18 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup as soup
 
+def help():
+
+    print( ''' open_session() : Ouvre une nouvelle séance de chrome avec une nouvelle adresse IP
+               close_session() : Fermer la session avant de reouvrir une nouvelle avec une nouvelle adresse IP (économise des ressources)
+               change(x) : Aller sur le site x avec une séance ouvert de chrome. Ex: change('https://www.google.com/') pour aller à Google.
+               data(): Lire le code html tel comme il est présenté dans le navigateur virtuel.
+               scroll(): Aller à la fin de la page.
+               screnshoot(x): Sauvegarder une capture d'écran du navigateur avec l'image x. Ex: screenshot('test.png')
+               screen(): Sauvegarder une capture d'écran du navigateur sous le nom 'browser.png'
+               find(x,y): Trouver tous les éléments avec les identifiants html x et y. Ex: identifiant= h2 class="mb0" -> x='h2' ; y={'class':'mb0'}
+            ''')
+
 def open_session():
     global browser
     PROXY = "127.0.0.1:24000"
@@ -17,6 +29,9 @@ def open_session():
 
 def screenshot(x):
     browser.save_screenshot(x)
+
+def screen():
+	browser.save_screenshot('browser.png')
 
 def close_session():
     browser.close()
@@ -40,12 +55,12 @@ def scroll():
 def change(x):
 	browser.get(x)
 
-class find(x,y):
+class find(x,y=None):
     def __init__(self):
         self.x = x
-        self.y = y
+        self.y = y if y is not None else x
     def now(self):
-        return sopa.findAll(a,b)
+        return sopa.findAll(x,y)
 
 def printext(x):
     for a in x:
